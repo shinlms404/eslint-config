f25734247891b35da2a2093a78a64437b7afde5c
cx <978563372@qq.com>
2024-01-22 17:09:54 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,7 @@
-import eslintConfigs from '@shinlms404/eslint-config'
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+require('sucrase/register')
+/** @type {typeof import('./src/index.ts')} */
+const { eslintConfigs } = require('./src/index.ts')
 
 export default eslintConfigs()
