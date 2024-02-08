29cc9664f60063e268a82900b488c3c0ba5067be
Charlie Chan <shinlms404@gmail.com>
2024-02-08 21:03:23 +0800

    chore: refactor isInEditor logic in env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,9 @@
 import process from 'node:process'
 
-export const isInEditor =
-  (process.env.VSCODE_PID || process.env.JETBRAINS_IDE) && !process.env.CI
+export const isInEditor = !!(
+  (process.env.VSCODE_PID ||
+    process.env.VSCODE_CWD ||
+    process.env.JETBRAINS_IDE ||
+    process.env.VIM) &&
+  !process.env.CI
+)
